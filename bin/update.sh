987763f8ddfe3e9e0b5a5f1d7e338ca42bb07969
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-31 14:29:39 +0000

    web folder contains lightbot

--- bin/update.sh
+++ bin/update.sh
@@ -6,13 +6,12 @@ website="../web"
 zipfile="../web/lightbot.zip"
 lightbot_folder="$website/lightbot.com"
 
-rm -rf "$lightbot_folder"
 mkdir -p "$website"
 
 (
   cd $website || { >&2 echo "ERROR: Could not find website directory \"$website\"." ; exit 1 ; }
   echo "downloading to `pwd`"
-  wget -p -k http://lightbot.com/hocflash.html
+  wget -c -N -p -k http://lightbot.com/hocflash.html
 ) || { >&2 echo "ERROR: could not download." ; exit 1 ; }
 
 echo "<html>
@@ -25,5 +24,5 @@ echo "<html>
 </html>" > "$lightbot_folder"/index.html
 
 rm -f $zipfile
-zip -r -9 -o -m $zipfile $lightbot_folder
+zip -r -9 -o $zipfile $lightbot_folder
 
